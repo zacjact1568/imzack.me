97edd78db3d0810ed440dedaaf482d4da2fb7adf
zacjact1568 <511470661@qq.com>
2018-01-10 20:29:45 +0800

    启用生产环境

--- imzack_me/settings.py
+++ imzack_me/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^gwxid)lelo_a@+yaz9@rn-yf%&0cvzoqh49qa8(x-&(wx=n30'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.imzack.me']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
